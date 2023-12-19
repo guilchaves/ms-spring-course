843e834aa758d73fc123c3ded838b4ce105ae7b7
guilherme <chaves.guilherme@proton.me>
2023-12-19 20:04:36 -0300

    feat(hr-payroll): add ribbon load balancing

--- hr-worker/src/main/java/br/com/guilchaves/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/guilchaves/hrworker/controllers/WorkerController.java
@@ -2,7 +2,10 @@ package br.com.guilchaves.hrworker.controllers;
 
 import br.com.guilchaves.hrworker.dto.WorkerDTO;
 import br.com.guilchaves.hrworker.services.WorkerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerService service;
 
@@ -26,6 +34,8 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<WorkerDTO> findById(@PathVariable Long id){
+        logger.info("PORT: " + env.getProperty("local.server.port"));
+
         WorkerDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
