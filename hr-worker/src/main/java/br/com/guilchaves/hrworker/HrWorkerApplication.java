aa91a51eb717f37dfe73d192b3471d1d27855a50
guilherme <chaves.guilherme@proton.me>
2023-12-21 16:18:03 -0300

    build(hr-eureka-server): config eureka server

--- hr-worker/src/main/java/br/com/guilchaves/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/br/com/guilchaves/hrworker/HrWorkerApplication.java
@@ -2,7 +2,9 @@ package br.com.guilchaves.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
@@ -11,3 +13,4 @@ public class HrWorkerApplication {
     }
 
 }
+
